11f619c0afbb922060727321903337a3c8920e99
Anders Pearson <anders@columbia.edu>
2013-01-23 15:36:07 -0500

    working out types

--- griddo/griddo.go
+++ griddo/griddo.go
@@ -1,9 +1,39 @@
 package griddo
 
 import (
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
 	"fmt"
+	"html/template"
 	"net/http"
+	"time"
 )
+
+type Grid struct {
+	Owner    *datastore.Key
+	Title    string
+	Created  time.Time
+	Modified time.Time
+}
+
+type Dimension struct {
+	Grid         *datastore.Key
+	Label        string
+	DisplayOrder int
+}
+
+type Slice struct {
+	Dimension    *datastore.Key
+	Label        string
+	DisplayOrder int
+}
+
+type Cell struct {
+	Value    int
+	Created  time.Time
+	Modified time.Time
+}
 
 func init() {
 	http.HandleFunc("/", handler)
